dddce613ed6c3c000cebf9c43a1ca4cb494252fa
SH-ke <SHke_dev.163.com>
2023-10-13 12:41:18 +0000

    patch barpadding

--- config.def.h
+++ config.def.h
@@ -7,6 +7,9 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int viewontag          = 1;     /* Switch view on tag switch */
+
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
  
 static const char *fonts[]          = { "nerd-fonts-fira-code:size=14" };
 static const char dmenufont[]       = "nerd-fonts-fira-code:size=14";
