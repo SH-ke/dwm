1703f162bc00f6e6e662e98f2c07cf56fc58b0ab
SH-ke <SHke_dev.163.com>
2024-02-26 16:24:52 +0800

    patch set nmaster

--- config.def.h
+++ config.def.h
@@ -104,6 +104,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_o,      resetnmaster,   {0} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
